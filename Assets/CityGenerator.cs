df569f648b18662fa460993cc5dfd78e746a7d5d
Kyle Dixon <90637263+KyleDixonVO@users.noreply.github.com>
2022-11-18 14:53:01 -0400

    Added sliders to adjust generation properties in the build. Building color is now based off of height.

--- Assets/CityGenerator.cs
+++ Assets/CityGenerator.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CityGenerator : MonoBehaviour
 {
@@ -23,17 +24,37 @@ public class CityGenerator : MonoBehaviour
     public GameObject cityOrigin;
     public GameObject building;
 
+    public Slider MaxBuildingHeight;
+    public Slider MinBuildingHeight;
+    public Slider MaxBuildingWidth;
+    public Slider MinBuildingWidth;
+    public Slider StreetSpaceMax;
+    public Slider StreetSpaceMin;
+
     public List<GameObject> buildings;
     // Start is called before the first frame update
     void Start()
     {
         buildings = new List<GameObject>();
         GenerateBuildings();
+        MaxBuildingHeight.value = maxBuildingHeight;
+        MinBuildingHeight.value = minBuildingHeight;
+        MaxBuildingWidth.value = maxBuildingWidth;
+        MinBuildingWidth.value = minBuildingWidth;
+        StreetSpaceMax.value = streetMaxSpacing;
+        StreetSpaceMin.value = streetMinSpacing;
     }
 
     // Update is called once per frame
     void Update()
     {
+        maxBuildingHeight = MaxBuildingHeight.value;
+        minBuildingHeight = MinBuildingHeight.value;
+        maxBuildingWidth = MaxBuildingWidth.value;
+        minBuildingWidth = MinBuildingWidth.value;
+        streetMaxSpacing = StreetSpaceMax.value;
+        streetMinSpacing = StreetSpaceMin.value;
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
             for(int i = 0; i < buildings.Count; i++)
@@ -66,20 +87,26 @@ public class CityGenerator : MonoBehaviour
                 building = GameObject.CreatePrimitive(PrimitiveType.Cube);
                 building.transform.position = new Vector3(cityOrigin.transform.position.x + generatedDistX, cityOrigin.transform.position.y, cityOrigin.transform.position.z + generatedDistZ);
                 building.transform.localScale = new Vector3(Random.Range(minBuildingWidth, maxBuildingWidth), Random.Range(minBuildingHeight, maxBuildingHeight), Random.Range(minBuildingWidth, maxBuildingWidth));
-                
-                int colorRng = Random.Range(0, 3);
 
-                if (colorRng == 0)
+                if (building.transform.localScale.y < 20)
                 {
                     building.GetComponent<MeshRenderer>().material.color = Color.cyan;
                 }
-                else if (colorRng == 1)
+                else if (building.transform.localScale.y < 60)
                 {
                     building.GetComponent<MeshRenderer>().material.color = Color.green;
                 }
-                else if (colorRng == 2)
+                else if (building.transform.localScale.y < 100)
                 {
                     building.GetComponent<MeshRenderer>().material.color = Color.yellow;
+                }
+                else if (building.transform.localScale.y < 150)
+                {
+                    building.GetComponent<MeshRenderer>().material.color = Color.red;
+                }
+                else
+                {
+                    building.GetComponent<MeshRenderer>().material.color = Color.magenta;
                 }
 
                 generatedDistZ += building.transform.localScale.z;
